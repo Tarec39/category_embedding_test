b8e1d24fc22b9f8b858df84fbef3c6252b89cfd4
Tarek Haabashy <tarekwk39@gmail.com>
2025-10-19 22:05:26 +0300

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-};
+  plugins: [],
+}
